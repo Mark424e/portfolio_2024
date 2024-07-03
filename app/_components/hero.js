b2fe298928142d70316cc9279efce625ce07b7e3
Mark <markphillip1800@gmail.com>
2024-07-03 22:21:20 +0200

    added gsap for animation purposes

--- app/_components/hero.js
+++ app/_components/hero.js
@@ -1,41 +1,68 @@
+'use client'
 import Image from 'next/image';
 import Skillsets from './skillsets';
+
+import React, { useRef } from 'react';
+import { gsap } from 'gsap';
+import { TextPlugin } from "gsap/TextPlugin";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(TextPlugin, ScrollTrigger);
 
 const OPTIONS = { loop: true };
 
 export const Hero = () => {
+  const boxRef = useRef(null);
+  useGSAP(() => {
+    gsap.defaults({ease: "none"});
+
+    const tl = gsap.timeline({repeat:0, repeatDelay:1, yoyo:false});
+    tl.to(".description", {duration: 4, text:"I develop responsive, user-friendly websites with a focus on clean, modern web design."})
+
+    const element = boxRef.current;
+
+    gsap.to(element, {
+      rotation: 360,
+      scrollTrigger: {
+        trigger: element,
+        start: 'top bottom',
+        end: '+=2000',
+        scrub: 2,
+      },
+    });
+  }, []);
+
   return (
     <section>
       <div className="container mx-auto pt-56">
         <div className='grid md:grid-cols-2 gap-8'>
-          <div className='text-center md:text-start'>
-            <h1 className="text-7xl md:text-5xl xl:text-7xl font-bold leading-none">
-              Hi, I'm
-              <span className="text-primary">
-              &nbsp; Mark
-              </span>
-            </h1>
-            <div>
-              <h1 className='text-6xl md:text-5xl xl:text-7xl font-bold leading-none'>Creative Frontend</h1>
-              <h1 className='text-6xl md:text-5xl xl:text-7xl font-bold leading-none'>Developer</h1>
-            </div>
-            <div className='mt-10 space-y-10'>
-              <p className='text-xl'>
-                I develop responsive, user-friendly websites with a focus on clean, modern web design.
-              </p>
+          <div className='relative text-center md:text-start'>
+            <div className='space-y-4'>
+              <h1 className="text-7xl md:text-5xl xl:text-7xl font-bold leading-none">
+                Hi, I'm
+                <span className="text-primary">
+                  &nbsp;Mark
+                </span>
+              </h1>
               <div>
-                <Skillsets options={OPTIONS} />
+                <h1 className='text-6xl md:text-5xl xl:text-7xl font-bold leading-none'>Creative Frontend Developer</h1>
               </div>
+              <p className='description absolute text-xl'></p>
+            </div>
+            <div className='md:absolute mt-24 md:bottom-0'>
+              <Skillsets options={OPTIONS} />
             </div>
           </div>
-          <div className='flex justify-center'>
+          <div className='relative flex justify-center'>
             <Image
-              className="floatingImage max-h-[500px]"
-              src="/hero-image-mark.webp"
+              className="floatingImage h-full"
+              src="/hero-image.webp"
               width={500}
               height={500}
               alt="Picture of the author"
             />
+            <div ref={boxRef} className="absolute w-80 h-80 bg-accent left-0 right-0 mx-auto top-[50%] translate-y-[-50%] z-[-9999]"></div>
           </div>
         </div>
       </div>
