8b6ad9fef00eb58800fc71ff307f687fd40d4b48
Mark <markphillip1800@gmail.com>
2024-07-04 14:48:20 +0200

    created logic for mapping projects in an array and rendering each one

--- app/_components/projects.js
+++ app/_components/projects.js
@@ -1,236 +1,114 @@
 import { React } from "react";
 import Image from "next/image";
 
+const projects = [
+  {
+    title: "React Weather Application",
+    description:
+      "A weather application that provides real-time weather updates, forecasts, and a responsive design for an optimal viewing experience.",
+    vercelLink: "https://weather-app-mark.vercel.app/",
+    githubLink: "https://github.com/Mark424e/react-weather-app",
+    technologies: ["React", "JavaScript", "Axios", "TailWindCSS"],
+    imageSrc: "/project1.webp",
+  },
+  {
+    title: "E-Commerce Webshop",
+    description:
+      "A refreshing lemonade webshop built with React. Features dynamic product displays, real-time inventory updates, and a sleek, responsive design.",
+    // vercelLink: "NO VERCEL LINK",
+    githubLink: "https://github.com/Mark424e/webshop",
+    technologies: ["PHP", "MySQL", "TailWindCSS"],
+    imageSrc: "/project4.webp",
+  },
+  {
+    title: "Lemonade Webshop",
+    description:
+      "A refreshing lemonade webshop built with React. Features dynamic product displays, real-time inventory updates, and a sleek, responsive design.",
+      vercelLink: "https://lemonadestand-mark.vercel.app/",
+    githubLink: "https://github.com/Mark424e/lemonadestand",
+    technologies: ["React", "JavaScript", "Axios", "TailWindCSS"],
+    imageSrc: "/project2.webp",
+  },
+  {
+    title: "Ice Cream Landingpage",
+    description:
+      "One of my very first projects made with pure markup, styling the elements with CSS, and utilizing SASS for more efficient and maintainable CSS.",
+    vercelLink: "https://gilleleje-isen.vercel.app/",
+    githubLink: "https://github.com/Mark424e/Gilleleje-Isen",
+    technologies: ["HTML", "CSS", "Sass"],
+    imageSrc: "/project1.webp",
+  },
+];
+
 export const Projects = () => {
   return (
     <div id="projects" className="container mx-auto">
       <ul className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-        <li>
-          <div className="bg-gray-950/50 border border-gray-100/25 transition hover:border-gray-200/50 p-4 rounded-xl">
-            <div className="grid xl:grid-cols-2 gap-4">
-              <div className="flex flex-col justify-between gap-8">
-                <div className="space-y-4">
-                  <h1 className="text-xl font-semibold">
-                    React Weather Application
-                  </h1>
-                  <p className="text-sm">
-                    A weather application that provides real-time weather
-                    updates, forecasts, and a responsive design for an optimal
-                    viewing experience.
-                  </p>
-                </div>
-                <div className="space-y-4">
-                  <div className="text-xs">
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://weather-app-mark.vercel.app/"
-                      target="_blank"
-                    >
-                      View on Vercel
-                    </a>
-                    <br />
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://github.com/Mark424e/react-weather-app"
-                      target="_blank"
-                    >
-                      View Github
-                    </a>
+        {projects.map((project, index) => (
+          <li key={index} className="flex">
+            <div className="bg-gray-950/50 border border-gray-100/25 transition hover:border-gray-200/50 p-4 rounded-xl flex flex-col flex-grow">
+              <div className="grid xl:grid-cols-2 gap-4 flex-grow">
+                <div className="flex flex-col justify-between gap-8 flex-grow">
+                  <div className="space-y-4">
+                    <h1 className="text-xl font-semibold">{project.title}</h1>
+                    <p className="text-sm">{project.description}</p>
                   </div>
-                  <div>
-                    <ul className="flex flex-wrap gap-2 text-sm text-text">
-                      <li className="bg-accent px-2 rounded-full">React</li>
-                      <li className="bg-accent px-2 rounded-full">Axios</li>
-                      <li className="bg-accent px-2 rounded-full">
-                        TailWindCSS
-                      </li>
-                    </ul>
+                  <div className="space-y-4">
+                    <div className="text-xs">
+                      {project.vercelLink && (
+                        <>
+                          <a
+                            className="nav-link text-gray-500 hover:text-text"
+                            href={project.vercelLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            View on Vercel
+                          </a>
+                          <br />
+                        </>
+                      )}
+                      {project.githubLink && (
+                        <a
+                          className="nav-link text-gray-500 hover:text-text"
+                          href={project.githubLink}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          View Github
+                        </a>
+                      )}
+                    </div>
+                    <div>
+                      <ul className="flex flex-wrap gap-2 text-sm text-text">
+                        {project.technologies.map((tech, idx) => (
+                          <li key={idx} className="bg-accent px-2 rounded-full">
+                            {tech}
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="transition md:hover:scale-95">
-                <a href="https://weather-app-mark.vercel.app/" target="_blank">
-                  <Image
-                    className="min-h-full object-cover rounded-xl"
-                    src="/project1.webp"
-                    width={1920}
-                    height={1080}
-                    alt="Project Image"
-                  />
-                </a>
+                <div className="transition md:hover:scale-95">
+                  <a
+                    href={project.vercelLink || project.githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Image
+                      className="min-h-full object-cover rounded-xl"
+                      src={project.imageSrc}
+                      width={1920}
+                      height={1080}
+                      alt={`${project.title} Image`}
+                    />
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-        </li>
-        <li className="hidden lg:block"></li>
-        <li className="hidden lg:block"></li>
-        <li>
-          <div className="bg-gray-950/50 border border-gray-100/25 transition hover:border-gray-200/50 p-4 rounded-xl">
-            <div className="grid xl:grid-cols-2 gap-4">
-              <div className="flex flex-col justify-between gap-8">
-                <div className="space-y-4">
-                  <h1 className="text-xl font-semibold">E-Commerce Webshop</h1>
-                  <p className="text-sm">
-                    A refreshing lemonade webshop built with React. Features
-                    dynamic product displays, real-time inventory updates, and a
-                    sleek, responsive design.
-                  </p>
-                </div>
-                <div className="space-y-4">
-                  <div className="text-xs">
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://lemonadestand-mark.vercel.app/"
-                      target="_blank"
-                    >
-                      View on Vercel
-                    </a>
-                    <br />
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://github.com/Mark424e/lemonadestand"
-                      target="_blank"
-                    >
-                      View Github
-                    </a>
-                  </div>
-                  <div>
-                    <ul className="flex flex-wrap gap-2 text-sm text-text">
-                      <li className="bg-accent px-2 rounded-full">PHP</li>
-                      <li className="bg-accent px-2 rounded-full">MySQL</li>
-                      <li className="bg-accent px-2 rounded-full">
-                        TailWindCSS
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="transition md:hover:scale-95">
-                <a href="https://weather-app-mark.vercel.app/" target="_blank">
-                  <Image
-                    className="min-h-full object-cover  rounded-xl"
-                    src="/project4.webp"
-                    width={1920}
-                    height={1080}
-                    alt="Project Image"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="bg-gray-950/50 border border-gray-100/25 transition hover:border-gray-200/50 p-4 rounded-xl">
-            <div className="grid xl:grid-cols-2 gap-4">
-              <div className="flex flex-col justify-between gap-8">
-                <div className="space-y-4">
-                  <h1 className="text-xl font-semibold">Lemonade Webshop</h1>
-                  <p className="text-sm">
-                    A refreshing lemonade webshop built with React. Features
-                    dynamic product displays, real-time inventory updates, and a
-                    sleek, responsive design.
-                  </p>
-                </div>
-                <div className="space-y-4">
-                  <div className="text-xs">
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://lemonadestand-mark.vercel.app/"
-                      target="_blank"
-                    >
-                      View on Vercel
-                    </a>
-                    <br />
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://github.com/Mark424e/lemonadestand"
-                      target="_blank"
-                    >
-                      View Github
-                    </a>
-                  </div>
-                  <div>
-                    <ul className="flex flex-wrap gap-2 text-sm text-text">
-                      <li className="bg-accent px-2 rounded-full">React</li>
-                      <li className="bg-accent px-2 rounded-full">Axios</li>
-                      <li className="bg-accent px-2 rounded-full">
-                        TailWindCSS
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="transition md:hover:scale-95">
-                <a href="https://weather-app-mark.vercel.app/" target="_blank">
-                  <Image
-                    className="min-h-full object-cover rounded-xl"
-                    src="/project2.webp"
-                    width={1920}
-                    height={1080}
-                    alt="Project Image"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </li>
-        <li className="hidden lg:block"></li>
-        <li className="hidden lg:block"></li>
-        <li>
-          <div className="bg-gray-950/50 border border-gray-100/25 transition hover:border-gray-200/50 p-4 rounded-xl">
-            <div className="grid xl:grid-cols-2 gap-4">
-              <div className="flex flex-col justify-between gap-8">
-                <div className="space-y-4">
-                  <h1 className="text-xl font-semibold">
-                    Ice Cream Landingpage
-                  </h1>
-                  <p className="text-sm">
-                    One of my very first projects made with pure markup, styling
-                    the elements with CSS, and utilizing SASS for more efficient
-                    and maintainable CSS
-                  </p>
-                </div>
-                <div className="space-y-4">
-                  <div className="text-xs">
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://gilleleje-isen.vercel.app/"
-                      target="_blank"
-                    >
-                      View on Vercel
-                    </a>
-                    <br />
-                    <a
-                      className="nav-link text-gray-500 hover:text-text"
-                      href="https://github.com/Mark424e/Gilleleje-Isen"
-                      target="_blank"
-                    >
-                      View Github
-                    </a>
-                  </div>
-                  <div>
-                    <ul className="flex flex-wrap gap-2 text-sm text-text">
-                      <li className="bg-accent px-2 rounded-full">HTML</li>
-                      <li className="bg-accent px-2 rounded-full">CSS</li>
-                      <li className="bg-accent px-2 rounded-full">Sass</li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="transition md:hover:scale-95">
-                <a href="https://weather-app-mark.vercel.app/" target="_blank">
-                  <Image
-                    className="min-h-full object-cover rounded-xl"
-                    src="/project1.webp"
-                    width={1920}
-                    height={1080}
-                    alt="Project Image"
-                  />
-                </a>
-              </div>
-            </div>
-          </div>
-        </li>
+          </li>
+        ))}
       </ul>
     </div>
   );
