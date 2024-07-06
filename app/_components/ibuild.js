2e40f4dbdb936247a1978081d06c512fadf0a4bd
Mark <markphillip1800@gmail.com>
2024-07-06 18:46:55 +0200

    what i build section

--- app/_components/ibuild.js
+++ app/_components/ibuild.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { FlipWords } from "./ui/flip-words";
 
 export const Ibuild = () => {
+  const words = ["optimized", "elegant", "outstanding", "beautiful", "modern"];
+
   return (
-    <div className="container mx-auto py-14 px-4 md:px-0">Ibuild</div>
-  )
-}
+    <div className="container mx-auto py-14 px-4 md:px-0">
+      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+        I build
+        <FlipWords words={words} />
+        websites
+      </div>
+    </div>
+  );
+};
